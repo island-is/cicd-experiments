8b2d0878a2840ae2390a18e340ea72bad42d96e4
lommi <jonorn@gmail.com>
2025-02-21 14:36:08 +0000

    Testiti test (#92)
    * fix: test
    
    * fix: test
    
    * fix: test
    
    * fix: ok
    
    * fix: test2
    
    * fix: test
    
    * fix: ok
    
    * fix: ok

---
+++ ci/test.mjs
@@ -0,0 +1,54 @@
+import github from "@actions/github";
+
+const context = github.context;
+const eventName = context.eventName;
+
+const targetBranch = getTargetBranch();
+const typeOfDeployment = getTypeOfDeployment();
+
+console.log({
+    targetBranch,
+    typeOfDeployment
+})
+
+
+
+function getTypeOfDeployment() {
+    if (targetBranch === 'main') {
+        return {
+            dev: true,
+            prod: false
+        }
+    }
+    if (targetBranch.startsWith('release')) {
+        return {
+            dev: false,
+            prod: true
+        }
+    }
+    // UNKNOWN BRANCH
+    console.error(`Unknown branch: ${targetBranch} - not sure how to tag this deployment`);
+    return {
+        dev: false,
+        prod: false
+    }
+}
+
+
+function getTargetBranch() {
+    if (eventName === "pull_request") {
+        return context.payload.pull_request?.base?.ref;
+    }
+    if (eventName === "push") {
+        return context.ref?.replace('refs/heads/', '');
+    }
+    if (eventName === "merge_group") {
+        return context.payload.merge_group.base_ref.replace('refs/heads/', '');
+    }
+    
+    if (process.env.GITHUB_BASE_REF) {
+        return process.env.GITHUB_BASE_REF;
+    }
+
+    throw new Error(`Unable to determine target branch for event type: ${eventName}`);
+}
